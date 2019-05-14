06f581f1de015817e22ed2c6531010c8004f0651
Code_Batman <45829036+Ishrak30@users.noreply.github.com>
2019-05-14 21:26:39 +0600

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,11 @@
+name = input("Give a name: ")
+age = input("Give a age: ")
+occupation = input("Give a occupation: ")
+role = input("Give a role: ")
+credential = input("Give a credential: ")
+
+print("His name is " + name)
+print("His age is " + str(age))
+print("His occupation is " + occupation + ". However he is working as " +role)
+print("But you have to know he has a degree in " + credential)
+
